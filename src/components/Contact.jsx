6fb789fc19f51df5c09b0ba439f4064b7aa9ae54
Mohammad Aman <107387923+mohdamangithub@users.noreply.github.com>
2026-09-11 22:37:14 +0530

    Update resume link in Contact component

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -93,7 +93,7 @@ const Contact = () => {
           variant="outlined"
           endIcon={<ArrowOutward />}
           component="a"
-          href="https://drive.google.com/file/d/1kWQfpgBD2Fio1b5qGG7IboJNBroPvPYN/view?usp=sharing"
+          href="https://drive.google.com/file/d/1hgG5Vit-HVrCY-t4-DcrR74f5V3WmVme/view?usp=sharing"
           target="_blank"
           rel="noopener noreferrer"
         >
